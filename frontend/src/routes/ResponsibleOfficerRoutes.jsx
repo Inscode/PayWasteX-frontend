de03516dc332af457e971284433b0ca527e62a9f
Tharushi Hansima <tharushihansima23@gmail.com>
2025-07-29 13:55:05 +0530

    Responsible officer ui changes

--- frontend/src/routes/ResponsibleOfficerRoutes.jsx
+++ frontend/src/routes/ResponsibleOfficerRoutes.jsx
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import ResponsibleOfficerDashboard from "../pages/ResponsibleOfficer/Dashboard";
 import Reports from "../pages/ResponsibleOfficer/Reports";
 import BillManagement from "../pages/ResponsibleOfficer/BillManagement";
-import PaymentConfirmation from "../pages/ResponsibleOfficer/PaymentConfirmation";
+import DirectPayment from "../pages/ResponsibleOfficer/DirectPayment";
+import Settings from "../pages/ResponsibleOfficer/Settings";
+import CustomerDetails from "../pages/ResponsibleOfficer/CustomerDetails";
 export default function ResponsibleOfficerRoutes() {
   return (
     <Routes>
       <Route path="dashboard" element={<ResponsibleOfficerDashboard />} />
       <Route path="reports" element={<Reports />} />   
       <Route path="billmanagement" element={<BillManagement />} />
-      <Route path="paymentconfirmation" element={<PaymentConfirmation />} />
+      <Route path="directpayments" element={<DirectPayment />} />
+      <Route path="settings" element={<Settings />} />
+      <Route path="customerdetails" element={<CustomerDetails />} />
+
       
       
         {/* Add more responsible officer routes here */}
